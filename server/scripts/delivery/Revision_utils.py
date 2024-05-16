9b6888df0cf8a6b29128e596e2ebdacdc08efab6
Silmaen <genteur.slayer@laposte.net>
2024-05-16 11:16:49 +0200

    add revision system (#1)

---
+++ server/scripts/delivery/Revision_utils.py
@@ -0,0 +1,85 @@
+"""
+Utility function for revision manipulation
+"""
+
+from .models import RevisionItemEntry
+
+
+def get_revision_branches():
+    revs = RevisionItemEntry.objects.order_by("-date")
+    if len(revs) == 0:
+        return []
+    result = []
+    for rev in revs:
+        result.append(rev.branch)
+    return list(set(result))
+
+
+def get_revision_hashes(branch_filter=""):
+    if branch_filter not in [None, ""]:
+        revs = RevisionItemEntry.objects.filter(branch=branch_filter).order_by("-date")
+    else:
+        revs = RevisionItemEntry.objects.order_by("-date")
+    if len(revs) == 0:
+        return []
+    result = []
+    for rev in revs:
+        if rev.hash not in result:
+            result.append(rev.hash)
+    return result
+
+
+def get_revision_count(branch_filter=""):
+    return len(get_revision_hashes(branch_filter))
+
+
+def get_revision_date(revision_hash):
+    revs = RevisionItemEntry.objects.filter(hash=revision_hash).order_by("date")
+    if len(revs) == 0:
+        return "no date"
+    return revs[0].date
+
+
+def get_revision_name_list(revision_hash):
+    revs = RevisionItemEntry.objects.filter(hash=revision_hash).order_by("-date")
+    if len(revs) == 0:
+        return []
+    names = []
+    for rev in revs:
+        names.append(rev.name)
+    names = list(set(names))
+    names.sort()
+    return names
+
+
+def get_revision_info(revision_hash):
+    revs = RevisionItemEntry.objects.filter(hash=revision_hash).order_by("date")
+    if len(revs) == 0:
+        return {}
+    current_revision = {
+        "hash": revs[0].hash,
+        "branch": revs[0].branch,
+        "date": revs[0].date,
+        "item_list": [],
+    }
+    for name in get_revision_name_list(revision_hash):
+        name_revs = revs.filter(name=name)
+        item = {
+            "name": name,
+            "type": name_revs[0].get_pretty_type(),
+            "icon": None,
+            "flavors": [],
+        }
+        f = []
+        for rev in name_revs:
+            f.append(
+                {
+                    "name": rev.flavor_name,
+                    "url": str(rev.package.path).replace("/data/", "/media/"),
+                    "size": rev.get_pretty_size_display(),
+                    "pk": rev.pk,
+                }
+            )
+        item["flavors"] = f
+        current_revision["item_list"].append(item)
+    return current_revision
